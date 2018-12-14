a523bfa460590a60de200bb2f877e037cd640101
niujt <670482466@qq.com>
2018-12-14 14:22:25 +0800

    first commit;

---
+++ src/main/java/demo/Demo2.java
@@ -0,0 +1,16 @@
+package demo;
+
+
+public class Demo2 {
+    public static void main(String args[]){
+        try {
+            JsonUtil.writeFile(
+                    JsonUtil.EmpList2Json(
+                            JsonUtil.changeString2Emp(
+                                    JsonUtil.readFileByLines("地址.txt"))).toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
